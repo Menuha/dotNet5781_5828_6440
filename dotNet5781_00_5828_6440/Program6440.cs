c1bc6e602717ae03c9895151c7a8ade22e669d74
shira cohen <coheshir@g.jct.ac.il>
2020-10-25 19:38:43 +0200

    Welcome+Hey

--- dotNet5781_00_5828_6440/Program6440.cs
+++ dotNet5781_00_5828_6440/Program6440.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace dotNet5781_00_5828_6440
 {
+
     partial class Program
     {
+        
         static partial void Welcome6440()
         {
             Console.WriteLine("I am also here");
+            Console.WriteLine("Hey");
         }
     }
 }
