5d0f554c735b94f115a3c8cab81d66f4f30945e2
shira cohen <coheshir@g.jct.ac.il>
2020-11-21 23:37:16 +0200

    change2

--- dotNet5781_02_5828_6440/BusStation.cs
+++ dotNet5781_02_5828_6440/BusStation.cs
@@ -1,4 +1,69 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Cryptography;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace dotNet5781_02_5828_6440
+//{
+//    public class BusStation
+//    {
+//        public int busStationKey;
+//        public float latitude;
+//        public float longitude;
+
+//        public BusStation(int buskode, float latitude, float longitude)
+//        {
+//            this.busStationKey = buskode;
+//            this.latitude = latitude;
+//            this.longitude = longitude;
+//        }
+//        public int BusStationKey
+//        {
+//            get
+//            {
+//                return busStationKey;
+//            }
+//            set
+//            {
+//                busStationKey = value;
+//            }
+//        }
+//        public float Latitude
+//        {
+//            get 
+//            {
+//                return latitude;
+//            }
+//            set
+//            {
+//                Random r1 = new random();
+//                latitude = r1.Next(310, 333);
+//                //latitude = latitude / 10;
+//            }
+//        }
+//        public float Longitude
+//        {
+//            get 
+//            {
+//                return longitude;
+//            }
+//            set 
+//            {
+//                Random r2 = new random();
+//                longitude = r2.Next(343, 355);
+//                //longitude = longitude / 10;
+//            }
+//        }
+//        public override string ToString()
+//        {
+//            return "Bus Station Code: " + busStationKey + ", " + latitude + "°N ," + longitude + "°E";
+//        }
+//    }
+
+//}
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -51,38 +116,25 @@ namespace dotNet5781_02_5828_6440
             }
             set
             {
-                if (value.Length != 6)
-                {
-                    //Exception
-                    Console.WriteLine("WRONG BUS STATION CODE");
-                    return;
-                }
-                stationCode = value;
+                busStationKey = value;
             }
         }
-
-        public double Latitude
+        public float Latitude
         {
-            get 
+            get
             {
                 return latitude;
             }
             set
             {
-                if (value < 31 || value > 33.3)
-                {
-                    //A random number in the width area of the State of Israel
-                    Random random = new Random(DateTime.Now.Millisecond);
-                    latitude = random.NextDouble() * (maxLatitude - minLatitude) + minLatitude;
-                    return;
-                }
-                latitude = value;
+                Random r1 = new random();
+                latitude = r1.Next(310, 333);
+                //latitude = latitude / 10;
             }
         }
-
-        public double Longitude
+        public float Longitude
         {
-            get 
+            get
             {
                 return longitude;
             }
