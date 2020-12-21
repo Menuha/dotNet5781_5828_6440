16a10c65e27c07128be9fb14889669eecc5b10e1
shira cohen <coheshir@g.jct.ac.il>
2020-12-21 11:58:18 +0200

    3B

---
+++ dotNet5781_03B_5828_6440/Bus3.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_03B_5828_6440
+{
+    enum Status { Ready = 1, InTravel, InRefueling, InTreat }
+    class Bus3
+    {
+            public Status myStatus = new Status();
+            public const int maxKmTreat = 20000;
+            public const int maxKmRefueling = 1200;
+
+            private string licenseNum;
+            private DateTime firstDate;
+            /// <summary>
+            /// kilometrage = kilometrage since the first day
+            /// </summary>
+            private int kilometrage;
+            /// <summary>
+            /// gas = kilometrage since the last refueling
+            /// </summary>
+            private int gas;
+            /// <summary>
+            /// lastTreatKm = kilometrage since the last treatment
+            /// </summary>
+            private int lastTreatKm;
+            private DateTime lastTreatDate;
+            public Bus3() { }
+            /// <summary>
+            /// Constractor of the Bus class
+            /// </summary>
+            /// <param name="licenseNum">The bus licunse number</param>
+            /// <param name="firstDate">The first date this bus started working</param>
+            public Bus3(string licenseNum, DateTime firstDate)
+            {
+                this.licenseNum = licenseNum;
+                this.firstDate = firstDate;
+                kilometrage = 0;
+                gas = 0;
+                lastTreatKm = 0;
+                LastTreatDate = DateTime.Now;
+            }
+
+        //public string LicenseNum
+        //{
+        //    get => licenseNum;
+        //    private set => licenseNum = value;
+        //}
+        public string LicenseNum
+        {
+            get
+            {
+                return licenseNum;
+            }
+            set
+            {
+                licenseNum = value;
+            }
+        }
+        public DateTime FirstDate
+        {
+            get 
+            { 
+                return firstDate; 
+            }
+            set
+            {
+                firstDate = value;
+            }
+        }
+        public int Kilometrage
+            {
+                get
+                {
+                    return kilometrage;
+                }
+                set
+                {
+                    if (value > 0)
+                    {
+                        LastTreatKm += value;
+                        gas += value;
+                        kilometrage += value;
+                    }
+                }
+            }
+        //public int Gas
+        //{
+        //    get => gas;
+        //    private set => gas = value;
+        //}
+        public int Gas
+        {
+            get
+            {
+                return gas;
+            }
+            set
+            {
+                gas = value;
+            }
+        }
+        //public int LastTreatKm
+        //{
+        //        get => lastTreatKm;
+        //        private set => lastTreatKm = value;
+        //}
+        public int LastTreatKm
+        {
+            get
+            {
+                return lastTreatKm;
+            }
+            set
+            {
+                lastTreatKm = value;
+            }
+        }
+        //public DateTime LastTreatDate
+        //{
+        //      get => lastTreatDate;
+        //      private set => lastTreatDate = value;
+        //}
+        public DateTime LastTreatDate
+        {
+            get
+            {
+                return lastTreatDate;
+            }
+            set
+            {
+                lastTreatDate = value;
+            }
+        }
+
+        /// <summary>
+        /// This method checks if refueling is needed to get going
+        /// </summary>
+        /// <param name="distance">This is the distance you want to travel on this bus.</param>
+        /// <returns>"true" if refueling is required</returns>
+        public bool IfRefueling(int distance)
+            {
+                if ((gas + distance) > maxKmRefueling)
+                {
+                    Console.WriteLine("Gas needed");
+                    return true;
+                }
+                return false;
+            }
+
+            /// <summary>
+            /// This method checks if general bus care is needed to get going.
+            /// </summary>
+            /// <param name="distance">This is the distance you want to travel on this bus.</param>
+            /// <returns>"true" if treatment is required</returns>
+            public bool IfTreat(int distance)
+            {
+                if ((LastTreatKm + distance) > maxKmTreat)
+                {
+                    Console.WriteLine("Please make a general treatment, this bus is DANGEROUS!");
+                    return true;
+                }
+                DateTime currentDay = DateTime.Now;
+                DateTime checkDate = new DateTime(LastTreatDate.Year + 1, LastTreatDate.Month, LastTreatDate.Day);
+                if (checkDate <= currentDay)
+                {
+                    Console.WriteLine("Please make a general treatment");
+                    return true;
+                }
+                return false;
+            }
+
+            /// <summary>
+            /// This method updates that refueling has been performed
+            /// </summary>
+            public void GasNow()
+            {
+                gas = 0;
+            }
+
+            /// <summary>
+            /// This method updates that a general bus treatment has been performed
+            /// </summary>
+            public void TreatNow()
+            {
+                LastTreatKm = 0;
+                LastTreatDate = DateTime.Now;
+            }
+    }
+
+}
+
+
--- dotNet5781_03B_5828_6440/MainWindow.xaml.cs
+++ dotNet5781_03B_5828_6440/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static dotNet5781_03B_5828_6440.Bus3;
 
 namespace dotNet5781_03B_5828_6440
 {
@@ -20,9 +21,109 @@ namespace dotNet5781_03B_5828_6440
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Bus3> busList;
         public MainWindow()
         {
             InitializeComponent();
+            createBusList();
+            bus3DataGrid.DataContext = busList;
+            bus3DataGrid.IsReadOnly = true;
+        }
+        void createBusList() 
+        {
+            busList = new List<Bus3>
+            {
+                new Bus3
+                {
+                    LicenseNum ="12-345-67",
+                    FirstDate=DateTime.Parse("1.1.2013"),
+                    Kilometrage=40000,
+                    Gas=111,
+                    LastTreatDate=DateTime.Parse("1.5.2020"),
+                    LastTreatKm=0
+                },
+                new Bus3
+                {
+                    LicenseNum ="12-345-88",
+                    FirstDate=DateTime.Parse("1.2.2010"),
+                    Kilometrage=50000,
+                    Gas=500,
+                    LastTreatDate=DateTime.Parse("1.1.2020"),
+                    LastTreatKm=19950
+                },
+                new Bus3
+                {
+                    LicenseNum ="12-345-99",
+                    FirstDate=DateTime.Parse("1.1.2010"),
+                    Kilometrage=70000,
+                    Gas=1190,
+                    LastTreatDate=DateTime.Parse("1.8.2020"),
+                    LastTreatKm=12000
+                },
+                new Bus3
+                {
+                    LicenseNum ="123-55-111",
+                    FirstDate=DateTime.Parse("1.3.2019"),
+                    Kilometrage=27000,
+                    Gas=100,
+                    LastTreatDate=DateTime.Parse("1.1.2020"),
+                    LastTreatKm=7500
+                },
+                new Bus3
+                {
+                    LicenseNum ="56-345-66",
+                    FirstDate=DateTime.Parse("1.1.2017"),
+                    Kilometrage=30000,
+                    Gas=230,
+                    LastTreatDate=DateTime.Parse("1.7.2020"),
+                    LastTreatKm=700
+                },
+                new Bus3
+                {
+                    LicenseNum ="22-245-67",
+                    FirstDate=DateTime.Parse("1.1.2016"),
+                    Kilometrage=30000,
+                    Gas=567,
+                    LastTreatDate=DateTime.Parse("1.1.2020"),
+                    LastTreatKm=7800
+                },
+                new Bus3
+                {
+                    LicenseNum ="124-45-671",
+                    FirstDate=DateTime.Parse("1.1.2019"),
+                    Kilometrage=4000,
+                    Gas=191,
+                    LastTreatDate=DateTime.Parse("1.1.2020"),
+                    LastTreatKm=7900
+                },
+                new Bus3
+                {
+                    LicenseNum ="12-345-45",
+                    FirstDate=DateTime.Parse("1.7.2011"),
+                    Kilometrage=80000,
+                    Gas=987,
+                    LastTreatDate=DateTime.Parse("1.11.2019"),
+                    LastTreatKm=15000
+                },
+                new Bus3
+                {
+                    LicenseNum ="12-135-88",
+                    FirstDate=DateTime.Parse("1.1.2008"),
+                    Kilometrage=120000,
+                    Gas=100,
+                    LastTreatDate=DateTime.Parse("1.1.2020"),
+                    LastTreatKm=12000
+                },
+                new Bus3
+                {
+                    LicenseNum ="121-34-677",
+                    FirstDate=DateTime.Parse("1.2.2018"),
+                    Kilometrage=2700,
+                    Gas=1000,
+                    LastTreatDate=DateTime.Parse("1.2.2020"),
+                    LastTreatKm=2700
+                }
+            };
         }
     }
 }
