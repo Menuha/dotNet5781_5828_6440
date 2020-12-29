31f361b5bf99cd5dcb2b9f9849f9edaed0dfbd1a
shira cohen <coheshir@g.jct.ac.il>
2020-12-29 16:36:21 +0200

    BO

---
+++ BL/BO/Line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Line:Station
+    {
+
+        //public IEnumerable<Station> StationsList { get; set; }
+
+    }
+}
