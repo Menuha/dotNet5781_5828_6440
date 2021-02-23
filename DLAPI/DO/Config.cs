88dd492e92591ce19e77b7dad8baf79cdb3414dd
Menuha <menuha@g.jct.ac.il>
2021-02-24 00:27:50 +0200

    AutoNums

---
+++ DLAPI/DO/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace DO
+    {
+        static class Config
+        {
+            //for running numbers
+            public static int LineID = 0;
+
+            //myLine.LineId =  Config.LineId++;
+
+            //for consts
+            public static int MAX_LICENSE_NUM = 8;
+            //...
+        }
+    }
+
+   
+
