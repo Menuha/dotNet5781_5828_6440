ada6cd7107cbeb5ec2311cf0ac619eb5843a0781
Menuha <menuha@g.jct.ac.il>
2021-02-24 16:00:41 +0200

    UpdateIndex

---
+++ PL/IndexWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for IndexWindow.xaml
+    /// </summary>
+    public partial class IndexWindow : Window
+    {
+        public BO.StationOfLine curSolBO;
+        public int indexBeforeUpdate;
+        public IndexWindow(BO.StationOfLine solBO, int routeLength)
+        {
+            InitializeComponent();
+            curSolBO = solBO;
+            indexBeforeUpdate = solBO.StationIndexInLine;
+
+            for (int i = 0; i < routeLength; i++)
+            {
+                cbIndex.Items.Add(i);
+            }
+
+            DataContext = curSolBO;
+        }
+
+        private void btUpdateIndex_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
