135df7d93dacd847c49f42f1be8c5b4b6c72ef22
shira cohen <coheshir@g.jct.ac.il>
2021-02-28 00:32:22 +0200

    XML files

--- DLAPI/DO/Exceptions.cs
+++ DLAPI/DO/Exceptions.cs
@@ -45,4 +45,17 @@ namespace DO
 
         public override string ToString() => base.ToString() + $", bad line id: {LineID} and station code: {StationCode}";
     }
+    public class XMLFileLoadCreateException : Exception
+    {
+        public string xmlFilePath;
+        public XMLFileLoadCreateException(string xmlPath) : base() { xmlFilePath = xmlPath; }
+        public XMLFileLoadCreateException(string xmlPath, string message) :
+            base(message)
+        { xmlFilePath = xmlPath; }
+        public XMLFileLoadCreateException(string xmlPath, string message, Exception innerException) :
+            base(message, innerException)
+        { xmlFilePath = xmlPath; }
+
+        public override string ToString() => base.ToString() + $", fail to load or create xml file: {xmlFilePath}";
+    }
 }
